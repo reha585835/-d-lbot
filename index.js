8077bde268d6625bd92e25896e2f3bb00fef3262
reha585835 <68445249+reha585835@users.noreply.github.com>
2020-12-01 20:00:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const proton = require("proton-io")
+const db = require("wio.db")
+const ayarlar = require("./ayarlar.json")
+
+const { Player } = require("discord-music-player")
+const player = new Player(client)
+client.player = player;
+
+let komutDosya = "./komutlar"
+let event = "./eventlar"
+let gelistiricler = ["773574463790383165"]
+let owner = true;
+let defaultCommands = ["ping", "yardım"]; 
+
+const komutYukle = new proton(client,komutDosya,event,gelistiricler,{owner,defaultCommands})
+
+let botlaraCevapVer = false;
+let etiketlePrefixOgren = true; 
+let etiketiPrefixOlarakKullan = true;
+
+client.on('message', async(msg) => {
+
+    if(!await db.has("prefix_" + msg.guild.id)) {
+       var prefix = "!"
+    } else {
+       var prefix = await db.fetch("prefix_" + msg.guild.id)
+    }
+
+    komutYukle.message(msg,prefix,{botlaraCevapVer,etiketiPrefixOlarakKullan,etiketlePrefixOgren})
+})
+
+client.on("message", (message) => {
+   if(message.content.toLowerCase() === "selam") {
+       message.channel.send("Aleyküm selam.")
+   }
+   if(message.content.toLowerCase() === "sea") {
+    message.channel.send("Aleyküm selam.")
+}
+})
+
+
+
+client.login("Nzc3ODkyNTY1ODUyNjE4Nzcy.X7KC6A.O9vBOEC8nV5L-lfmijrWfcIgFs4")
